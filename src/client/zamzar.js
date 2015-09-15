76198f45688060f9dac28c733a117dcd48d4daef
shaosh <shaosh0913@gmail.com>
2015-09-15 02:37:02 -0700

    Remove Express, finish box view, and zamzar conversion.

---
+++ src/client/zamzar.js
@@ -0,0 +1,109 @@
+var request = require('../../node_modules/request'),		
+		validurl = require('../../node_modules/valid-url'),
+		Q = require('../../node_modules/q'),
+		mkdirp = require('../../node_modules/mkdirp'),
+		fs = require('fs'),
+		//TODO: Update apikey
+		apiKey = '84a2e87d63c6a4f70aa6d232e1dc35e938f11d2a',
+		jobUrl = 'https://sandbox.zamzar.com/v1/jobs/',
+		fileUrl = 'https://sandbox.zamzar.com/v1/files/',
+		formData ={
+			source_format: 'cdr',
+			target_format: 'pdf',
+		},
+		interval = 1 * 1000;
+
+var zamzar = {
+	convert: function(file, filename){
+		var stream = _createReadStream(file);
+		formData.source_file = stream;
+		return _startConversion()
+			.then(function(res){return _waitConversion(res);}, function(e){_outputError(e);})
+			.then(function(res){return _downloadFile(res, filename);}, function(e){_outputError(e);});
+	}
+};
+
+var _createReadStream = function(file){
+	// Create a readstream from the file input
+	// File input could be a url or filepath
+	var uri = validUrl.is_web_uri(file);
+	if (uri){
+		return request(uri);
+	} else {
+		return fs.createReadStream(file);
+	}
+};
+
+var _startConversion = function(formData){
+	var deferred = Q.defer();
+	request.post({url:jobUrl, formData: formData}, function (err, response, body) {
+    if (!!err) {
+  		var error = 'Unable to start conversion job' + err;
+      deferred.rejected(error);
+    } 
+    else {
+      console.log('SUCCESS! Conversion job started:', JSON.parse(body));
+      deferred.resolve(JSON.parse(body));
+    }
+	}).auth(apiKey, '', true);
+	return deferred.promise;
+};
+
+var _waitConversion = function(data){
+	var jobid = data.id,
+			deferred = Q.defer();
+
+	var checkConversion = function(){
+		request.get (jobUrl + jobid, function (err, response, body) {
+	    if (err) {
+	    	var error = 'Still converting...' + err;
+      	deferred.rejected(error);
+	    } 
+	    else {
+	  		window.clearInterval();
+	  		console.log('SUCCESS! Conversion job completeded:', JSON.parse(body));
+	  		deferred.resolve(JSON.parse(body));
+	    }
+		}).auth(apiKey, '', true);
+	};
+	setInterval(checkConversion, interval);
+	return deferred.promise;
+};
+
+var _downloadFile = function(data, filename){
+	var fileid = data.target_files[0].id,
+			foldername = 'tmp',
+			localFilename = foldername + '/' + filename,
+			deferred = Q.defer();			
+	mkdirp.sync(foldername);
+	var r = request.get({url: fileUrl + fileID + '/content', followRedirect: false}, function (err, response, body) {
+    if (err) {
+    	var error = 'Unable to download file:' + err;
+      deferred.rejected(error);
+    } 
+    else {
+      if (response.statusCode === 307) {
+        // Download file from the redirect link
+        var fileRequest = request(response.headers.location);
+        fileRequest.on('response', function (res) {
+            res.pipe(fs.createWriteStream(localFilename));
+        });
+        fileRequest.on('end', function () {
+            console.log('File download complete');
+            deferred.resolve(localFilename);
+        });
+      }
+    }
+	}).auth(apiKey,'',true).pipe(fs.createWriteStream(localFilename));
+	r.on('end', function(){
+		console.log('File download complete');
+    deferred.resolve(localFilename);
+	});
+	return deferred.promise;
+};
+
+var _outputError = function(err){
+	console.error(err);
+}
+
+module.exports = zamzar;
